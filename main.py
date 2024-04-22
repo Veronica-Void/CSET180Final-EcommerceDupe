29a2e486d4c048fb7b8510c4e03dac6ef93ac4df
JaidenDenlinger <jaidendenlinger05@gmail.com>
2024-04-22 07:54:49 -0400

    Added notes in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
+from flask_mysqldb import MySQL # Gives flask extensions for MySQL making some work easier.
+import MySQLdb.cursors # Imports 'cursors' allows you to interect with MySQL database. Also used to execute SQL queries and fetch data from database.
+import re # Provide support for regular expressions, searches and manipulates strings, it helps with a lot of tasks like validation.
 from flask import Flask, render_template, request, redirect, session, url_for #imported flask and other things here
 from sqlalchemy import create_engine, text
 import hashlib
