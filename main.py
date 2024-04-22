912bbbe57aee5bd7da94686cd84f35c3ce8a074f
Veronica-Void <vvoid8075@gmail.com>
2024-04-22 11:23:03 -0700

    Added html+css, also created register function

--- main.py
+++ main.py
@@ -4,23 +4,48 @@ import hashlib
 
 c_str = "mysql://root:MySQL8090@localhost/ecomm"
 engine = create_engine(c_str, echo=True)
-connection = engine.connect
+conn = engine.connect
 
 app = Flask(__name__)
 app.secret_key = 'hola'
-
-
-
 
 @app.route('/')
 def home():
     return render_template('/index.html')
 
+# ------------------------------------------------ Start of Register ------------------------------------------------------------
+def hash_password(inputpw):
+    return hashlib.sha3_256(inputpw.encode())
+
+
+@app.route('/register', methods=['GET'])
+def showRegister():
+    return render_template('/register.html')
+
+
+@app.route('/register', methods=['POST'])
+def registerUser():
+    user_name = request.form.get('USER_NAME')
+    name = request.form.get('NAME')
+    email = request.form.get('EMAIL')
+    password = request.form.get('PASSWORD')
+    hashed_password = hash_password(password).hexdigest()
+    conn.execute(text(f'INSERT INTO `USER` (USER_NAME, `NAME`, EMAIL, `PASSWORD`, ACCOUNT_TYPE) VALUES (\'{user_name}\', \'{name}\',\'{email}\',\'{password}\'), ""'))
+    conn.commit
+    return redirect(url_for('showLogin'))
+# ------------------------------------------------ End of Register ------------------------------------------------------------
 
 
 
 
+# ------------------------------------------------ Start of Login ------------------------------------------------------------
+@app.route('/login', methods=['GET'])
+def showLogin():
+    return render_template('/login.html')
 
+# @app.route('/login', methods=['POST'])
+# def loginUser():
+# ------------------------------------------------ End of Login ------------------------------------------------------------
 
 
 
