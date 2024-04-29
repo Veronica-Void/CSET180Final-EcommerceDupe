e48968ece80c71d9e4f8829de206a5899f6a9ab3
Veronica-Void <vvoid8075@gmail.com>
2024-04-28 17:20:54 -0700

    moved a comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # i keep getting a yellow error whenever I uncomment this import - Vee
-# from flask_mysqldb import MySQL 
-# Gives flask extensions for MySQL making some work easier.
+# from flask_mysqldb import MySQL i# Gives flask extensions for MySQL making some work easier.
 
 import MySQLdb.cursors # Imports 'cursors' allows you to interect with MySQL database. Also used to execute SQL queries and fetch data from database.
 import re # Provide support for regular expressions, searches and manipulates strings, it helps with a lot of tasks like validation.
