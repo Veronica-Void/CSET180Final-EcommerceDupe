8a805bd818eacf1fabd0964d9893b864832ce3e3
Kishaun13 <Kishaunbrown14@gmail.com>
2024-05-02 11:17:53 -0400

    Deleted lines used fir testing when first creating my functions.

--- main.py
+++ main.py
@@ -235,9 +235,9 @@ def add_products_post():
 
 
     # Ensure the user exists in the USERS table
-    user_exists = conn.execute(text('SELECT * FROM USERS WHERE USER_NAME = :username'), {'username': created_by}).fetchone() is not None
-    if not user_exists:
-        conn.execute(text('INSERT INTO USERS (USER_NAME, NAME) VALUES (:username, :name)'), {'username': created_by, 'name': 'Test User'})
+    # user_exists = conn.execute(text('SELECT * FROM USERS WHERE USER_NAME = :username'), {'username': created_by}).fetchone() is not None
+    # if not user_exists:
+    #     conn.execute(text('INSERT INTO USERS (USER_NAME, NAME) VALUES (:username, :name)'), {'username': created_by, 'name': 'Test User'})
 
     conn.execute(text('INSERT INTO PRODUCT (TITLE, DESCRIPTION, WARRANTY_PERIOD, NUMBER_OF_ITEMS, PRICE, ADDED_BY_USERNAME, Category) VALUES (:title, :description, :warranty_period, :number_of_items, :price, :created_by, :category)'), {'title': request.form['title'], 'description': request.form['description'], 'warranty_period': request.form['warranty_period'], 'number_of_items': request.form['number_of_items'], 'price': request.form['price'], 'created_by': created_by, 'category': request.form['category']})
     conn.execute(text('INSERT INTO ProductImages (PID, imagesURL) VALUES (LAST_INSERT_ID(), :imagesURL)'), {'imagesURL': request.form['imagesURL']})
